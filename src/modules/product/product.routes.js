35de6eaca323cc0d788edd674dc9390de89a0dbb
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-08 01:41:19 -0600

    feat: allow employee to read products

--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/", protect, authorize("admin", "manager"), createProduct);
 router.get("/low-stock", protect, getLowStockProducts);
-router.get("/", protect, getProducts);
+router.get("/", protect,authorize("admin", "manager", "employee") ,getProducts);
 router.get("/:id", protect, getProductById);
 router.put("/:id", protect, authorize("admin", "manager"), updateProduct);
 router.delete("/:id", protect, authorize("admin", "manager"), deleteProduct);
