1e26bf5901f88a89d2a83bc5b491253614af7f61
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-17 01:49:26 -0500

    chore(deps): uplode jasmine and karma

--- inventory-frontend/src/app/app.routes.ts
+++ inventory-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginModule } from './features/auth/login/login-module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginModule },
+    { path : '', redirectTo: 'login', pathMatch: 'full' },
+];
