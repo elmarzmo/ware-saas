0d9a805668c93567e1831dfe6f54402b748b2be0
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-18 02:16:35 -0500

    feat(auth): implement login form withAPI integration

--- inventory-frontend/src/app/app.config.ts
+++ inventory-frontend/src/app/app.config.ts
@@ -3,12 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient()
+    ReactiveFormsModule,
+    
+    provideHttpClient(),
+    provideRouter(routes)
   ]
 };
+
--- inventory-frontend/src/app/features/auth/login/login.ts
+++ inventory-frontend/src/app/features/auth/login/login.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../../core/services/auth';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  email = '';
+  password = '';
+  organizationId = '';
+
+
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) { }
+
+  onLogin() {
+    this.auth.login({
+      email: this.email,
+      password: this.password,
+      organizationId: this.organizationId
+    }).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        alert('Login failed');
+      }
+    });
+  }
 }
