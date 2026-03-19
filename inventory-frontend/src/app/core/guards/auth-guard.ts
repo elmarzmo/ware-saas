f8ee7cf160fc0d8dd682e8f43d0037bd7d33af38
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-19 00:36:28 -0500

    feat(auth): protect dashboard with auth guard

--- inventory-frontend/src/app/core/guards/auth-guard.ts
+++ inventory-frontend/src/app/core/guards/auth-guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { Inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn =() => {
+
+  const router = Inject(Router);
+  const token = localStorage.getItem('token');
+  if (token) {
+    return true;
+  }
+  router.navigate(['/login']);
+  return false;
 };
