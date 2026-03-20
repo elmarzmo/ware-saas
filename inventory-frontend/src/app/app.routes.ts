35d7217d11463f8201aea5bea491c0e43d37c104
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-20 11:47:01 -0500

    feat(layout): wrap protected routes with layout

--- inventory-frontend/src/app/app.routes.ts
+++ inventory-frontend/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import {Login} from "./features/auth/login/login";
 import {Dashboard} from "./features/dashboard/dashboard";
 import {authGuard} from "./core/guards/auth-guard";
+import {Layout} from "./core/layout/layout";
 
 export const routes: Routes = [
     { path: 'login', component: Login },
-    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+    { path: '', component: Layout, canActivate: [authGuard], children: [
+        { path: 'dashboard', component: Dashboard }
+    ]},
     { path : '', redirectTo: 'login', pathMatch: 'full' },
 ];
