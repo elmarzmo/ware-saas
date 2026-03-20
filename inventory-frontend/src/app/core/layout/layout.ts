ee30a3fe9b1bd93f7c1482228a602eb3a1ccd561
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-20 11:44:15 -0500

    feat(layout): implement sidebar and navbar UI

--- inventory-frontend/src/app/core/layout/layout.ts
+++ inventory-frontend/src/app/core/layout/layout.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
 export class Layout {
+  constructor(private router: Router) {}
+
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 
 }
