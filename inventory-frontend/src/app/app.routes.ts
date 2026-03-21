cd6a4470f05ee29a0beea2a2f81f4ab81c4768e4
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-21 01:12:59 -0500

    feat(products): implement product CRUD UI

--- inventory-frontend/src/app/app.routes.ts
+++ inventory-frontend/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import {Login} from "./features/auth/login/login";
 import {Dashboard} from "./features/dashboard/dashboard";
 import {authGuard} from "./core/guards/auth-guard";
 import {Layout} from "./core/layout/layout";
-import { Products } from './features/products/products';
+import { Product } from './core/services/product';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: '', component: Layout, canActivate: [authGuard], children: [
         { path: 'dashboard', component: Dashboard },
-        { path: 'products', component: Products }
+        { path: 'products', component: Product }
     ]},
     { path : '', redirectTo: 'login', pathMatch: 'full' },
 ];
--- inventory-frontend/src/app/features/products/products.ts
+++ inventory-frontend/src/app/features/products/products.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../core/services/product';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [  ],
   templateUrl: './products.html',
   styleUrl: './products.scss',
 })
-export class Products {
+export class Products implements OnInit {
+
+  products: any[] = [];
+
+  newProduct = {
+    name: '',
+    sku: '',
+    quantity: 0,
+  };
+
+  constructor(private productService: Product) { }
+
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe((res: any) => {
+      this.products = res;
+    });
+  }
+
+  createProduct() {
+    this.productService.createProduct(this.newProduct).subscribe(() => {
+      this.newProduct = { name: '', sku: '', quantity: 0 };
+      this.loadProducts();
+    });
+  }
+
+    deleteProduct(id: string) {
+      if (confirm('Are you sure you want to delete this product?')) {
+        this.productService.deleteProduct(id).subscribe(() => {
+          this.loadProducts();
+        });
+      }
+    }
+
+    getLowStockProducts() {
+      this.productService.getLowStockProducts().subscribe((res: any) => {
+        this.products = res;
+      });
+    }
+
+    updateProduct(id: string, data: any) {
+      this.productService.updateProduct(id, data).subscribe(() => {
+        this.loadProducts();
+      });
+    }
+
+    getProductById(id: string) {
+      this.productService.getProductById(id).subscribe((res: any) => {
+        console.log(res);
+      });
+    }
+    
+
 
 }
