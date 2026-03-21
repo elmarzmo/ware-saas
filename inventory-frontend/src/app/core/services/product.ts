5aa1b0f98f9387ac8858ef0df3f5f8312d0ada0a
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-21 00:19:43 -0500

    feat(products): secure API calls with JWT

--- inventory-frontend/src/app/core/services/product.ts
+++ inventory-frontend/src/app/core/services/product.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Auth } from './auth';
 
 @Injectable({
   providedIn: 'root',
@@ -8,25 +9,28 @@ import { HttpClient } from '@angular/common/http';
 export class Product {
   private api = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private auth: Auth
+  ) {}
 
   getProducts() {
-    return this.http.get(`${this.api}/products`);
+    return this.http.get(`${this.api}/products`, { headers: this.auth.getAuthHeaders() });
   }
   getProductById(id: string) {
-    return this.http.get(`${this.api}/products/${id}`);
+    return this.http.get(`${this.api}/products/${id}`, { headers: this.auth.getAuthHeaders() });
   }
   getLowStockProducts() {
-    return this.http.get(`${this.api}/products/low-stock`);
+    return this.http.get(`${this.api}/products/low-stock`, { headers: this.auth.getAuthHeaders() });
   }
   createProduct(data: any) {
-    return this.http.post(`${this.api}/products`, data);
+    return this.http.post(`${this.api}/products`, data, { headers: this.auth.getAuthHeaders() });
   }
   updateProduct(id: string, data: any) {
-    return this.http.put(`${this.api}/products/${id}`, data);
+    return this.http.put(`${this.api}/products/${id}`, data, { headers: this.auth.getAuthHeaders() });
   }
   deleteProduct(id: string) {
-    return this.http.delete(`${this.api}/products/${id}`);
+    return this.http.delete(`${this.api}/products/${id}`, { headers: this.auth.getAuthHeaders() });
   }
 
   
