dc3529fa7a543e3329c2f645af2b8337983f43ea
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-22 13:25:36 -0500

    feat(Stock): implement stock movement simple UI

---
+++ inventory-frontend/src/app/core/services/stockService.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+
+import { StockService } from './stockService';
+
+describe('StockService', () => {
+  let service: StockService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StockService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- inventory-frontend/src/app/features/stock/stock.ts
+++ inventory-frontend/src/app/features/stock/stock.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockService } from '../../core/services/stockService';
+import { Product } from '../../core/services/product';
+
 
 @Component({
   selector: 'app-stock',
@@ -6,6 +9,42 @@ import { Component } from '@angular/core';
   templateUrl: './stock.html',
   styleUrl: './stock.scss',
 })
-export class Stock {
+export class Stock implements OnInit {
 
+  products: any[] = [];
+  movements: any[] = [];
+
+  newMovement = {
+    productId: '',
+    type: 'IN',
+    quantity: 0,
+  };
+
+  constructor(
+    private stockService: StockService,
+    private productService: Product
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+    this.loadMovements();
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe((res: any) => {
+      this.products = res;
+    });
+  }
+
+  loadMovements() {
+    this.stockService.getMovements().subscribe((res: any) => {
+      this.movements = res;
+    });
+  }
+
+  addMovement() {
+    this.stockService.createMovement(this.newMovement).subscribe(() => {
+      this.loadMovements();
+    });
+  }
 }
