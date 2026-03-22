6abe6f80b956fa1994e4763163734f6f7438cbb8
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-22 13:11:03 -0500

    feat(stock): add stock service

---
+++ inventory-frontend/src/app/core/services/stock.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Auth } from './auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Stock {
+  private api = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+    private auth: Auth
+  ) {}
+
+  createMovement(data: any) {
+    return this.http.post(`${this.api}/stock`, data, { headers: this.auth.getAuthHeaders() });
+  }
+  
+  getMovements() {
+    return this.http.get(`${this.api}/stock`, { headers: this.auth.getAuthHeaders() });
+  }
+  
+
+  
+}
