fd6c7cf9eef057f999bb3d639bc54bade8e379c5
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-24 00:47:14 -0500

    feat(analytics): add analytics service

---
+++ inventory-frontend/src/app/core/services/analytics-service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Auth } from './auth';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnalyticsService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient, private auth: Auth) {}
+
+  topProducts() {
+    return this.http.get(`${this.apiUrl}/analytics/top-moving-products`, {
+      headers: this.auth.getAuthHeaders(),
+    });
+  }
+
+  activeUsers() {
+    return this.http.get(`${this.apiUrl}/analytics/active-users`, {
+      headers: this.auth.getAuthHeaders(),
+    });
+  }
+
+  stockTrends() {
+    return this.http.get(`${this.apiUrl}/analytics/stock-trends`, {
+      headers: this.auth.getAuthHeaders(),
+    });
+  }
+  
+}
