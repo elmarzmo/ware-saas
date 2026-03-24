69485b820b982c9d42e15db257b909153590d580
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-24 01:11:06 -0500

    feat(analytics): implement analytics dashboard with charts

--- inventory-frontend/src/app/features/analytics/analytics.ts
+++ inventory-frontend/src/app/features/analytics/analytics.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from '../../core/services/analytics-service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {  ChartConfiguration } from 'chart.js/auto';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-analytics',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './analytics.html',
   styleUrl: './analytics.scss',
 })
-export class Analytics {
+export class Analytics implements OnInit {
 
+  topProductsChart!: ChartConfiguration<'bar'>;
+  activeUsersChart!: ChartConfiguration<'bar'>;
+  stockTrendsChart!: ChartConfiguration<'line'>;
+
+  constructor(private analyticsService: AnalyticsService) {}
+
+  ngOnInit(): void {
+    this.loadTopProducts();
+    this.loadActiveUsers();
+    this.loadStockTrends();
+  }
+
+  loadTopProducts() {
+    this.analyticsService.topProducts().subscribe((res: any) => {
+      this.topProductsChart  = {
+        type: 'bar',
+         data: {
+         
+          labels: res.map((item: any) => item.productName),
+          datasets: [
+            {
+              label: 'Total Moved',
+              data: res.map((item: any) => item.totalMoved),
+            }]
+        }
+      };
+    });
+  }
+
+  loadActiveUsers() {
+    this.analyticsService.activeUsers().subscribe((res: any) => {
+      this.activeUsersChart = {
+        type: 'bar',
+        data: {
+          labels: res.map((item: any) => item.username),
+          datasets: [
+            {
+              label: 'Active Sessions',
+              data: res.map((item: any) => item.activeSessions),
+            }]
+        }   
+
+      };
+    }); 
+  }
+
+  loadStockTrends() {
+    this.analyticsService.stockTrends().subscribe((res: any) => {
+      this.stockTrendsChart = {
+       
+        type: 'line',
+        data: {
+         
+          labels: res.map((item: any) => item.date),
+          datasets: [
+            {
+              label: 'Stock Level',
+              data: res.map((item: any) => item.stockLevel),
+            }]
+        }
+      };
+    }
+    );
+
+
+
+
+
+
+  }
 }
