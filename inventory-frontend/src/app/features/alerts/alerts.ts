045e9f7543955897e4fc1c1e5b2d88d6a02de60f
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-26 10:58:16 -0500

    feat(alerts): implement low-stock alerts dashboard

--- inventory-frontend/src/app/features/alerts/alerts.ts
+++ inventory-frontend/src/app/features/alerts/alerts.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../core/services/product';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-alerts',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './alerts.html',
   styleUrl: './alerts.scss',
 })
-export class Alerts {
+export class Alerts implements OnInit {
+
+  lowStockProducts: any[] = [];
+
+  constructor(private productService: Product) {}
+
+  ngOnInit() {
+    this.loadLowStockProducts();
+  }
+
+  loadLowStockProducts() {
+    this.productService.getLowStockProducts().subscribe((res: any) => {
+      this.lowStockProducts = res;
+    }
+    );
+  }
 
 }
