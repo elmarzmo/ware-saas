57e423a659a9f5883ebe7a0f082ff000ba50764e
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-03-31 23:00:45 -0500

    feat(auth-gard): check the request URL and reqrites the root / to login or dashboard

--- inventory-frontend/src/app/core/guards/auth-guard.ts
+++ inventory-frontend/src/app/core/guards/auth-guard.ts
@@ -1,11 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (_route, state) => {
   const router = inject(Router);
   const token = localStorage.getItem('token');
 
   if (token) {
+    if (state.url === '' || state.url === '/') {
+      return router.parseUrl('/dashboard');
+    }
     return true;
   }
 
